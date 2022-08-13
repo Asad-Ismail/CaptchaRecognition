a50483ae31aa49b06abb8fab0bf8a3fab4aa4b43
Asad Ismail <asadismaeel@gmail.com>
2022-08-13 20:38:38 +0200

    Checked first version with manual input Worksgit status

--- automated_captcha.py
+++ automated_captcha.py
@@ -19,74 +19,77 @@ links=["https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locat
 def send_message(subject='Nothing',body="Nothing to Update"):
     # Define email sender and receiver
     email_sender = 'asadismaeel@gmail.com'
-    email_password = '.NETALICE12'
+    email_password = 'mgfhptlydaoqhhog'
     email_receiver = 'asadismaeel@gmail.com'
-
     # Set the subject and body of the email
     #subject = 'Appointment Update'
     #body = """
     #I've just published a new video on YouTube: https://youtu.be/2cZzP9DLlkg
     #"""
-
     em = EmailMessage()
     em['From'] = email_sender
     em['To'] = email_receiver
     em['Subject'] = subject
     em.set_content(body)
-
     # Add SSL (layer of security)
     context = ssl.create_default_context()
-
     # Log in and send the email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_receiver, em.as_string())
+        
 
-def find_text(content,txt="Please enter here the text you see"):
-    if content.find(txt):
+def find_text(content,txt):
+    if txt in content:
         return True
-        #print("*"*100) 
-        #print("Given is present in the webpage")
     else:
-        False
+        return False
 
 def getcaptha(link,outfile):
+    # To run wothout opening the chrome
+    #op = webdriver.ChromeOptions()
+    #op.add_argument('headless')
+    #driver = webdriver.Chrome(PATH,options=op)
     driver=webdriver.Chrome(PATH)
     driver.get(link)
     with open(outfile, 'wb') as file:
         file.write(driver.find_element(By.XPATH,"//form[@id='appointment_captcha_month']/div/captcha/div").screenshot_as_png)
     ## Crop unneccassy part
     img=cv2.imread(outfile)
-    print(img.shape)
+    #print(img.shape)
     assert img is not None, "Input image is None"
     img=img[:,:300]
     sleep(1)
     content=driver.page_source
     status=find_text(content=content,txt="Please enter here the text you see")
-    assert status==Trues
+    assert status==True, "WebPage Could Not be Found!!"
     
     ## Run the model and get prediction as string in outstr
     #driver.close()
-    #inp=input("Enter the Captcha now!!")
+    outstr=input("Enter the Captcha now!!")
     #print(f"You entered {inp}")
     #print(f"Getting Text Box")
     
     textbox=driver.find_element(By.ID,"appointment_captcha_month_captchaText")
     textbox.send_keys(outstr)
-    sleep(1)
+    sleep(3)
     print(f"Sending predicted Captchas!!")
     submit = driver.find_element(By.ID,"appointment_captcha_month_appointment_showMonth")
     submit.click()
-    sleep(1)
+    sleep(5)
+    #content=""
     content=driver.page_source
+    #print(content)
     status=find_text(content=content,txt="Please enter here the text you see")
     foundappoint=find_text(content=content,txt="Unfortunately, there are no appointments")
+    sleep(5)
     # status 0 capthca read failes, 1 did not found an appointment, 2 found an appointment
+    #print(f"Intital status are {status}, {foundappoint}")
     if status:
         return 0
-    if !status and foundappoint:
+    if not status and foundappoint:
         return 1
-    if !status and !foundappoint:
+    if not status and not foundappoint:
         return 2
     
 
@@ -96,7 +99,8 @@ foundappointment=False
 checked_links=0
 for link in links:
     for _ in range(MAX_TRYS):
-        status=getcaptha(link,os.path.join(dst_data,str(i)+".png"))
+        status=getcaptha(link,os.path.join(dst_data,"test"+".png"))
+        #print(status)
         if status==1:
             checked_links+=1
             break
@@ -104,14 +108,14 @@ for link in links:
             checked_links+=1
             foundappointemnt=True
             break
-    time.sleep(2)
+    time.sleep(1)
     
 
-
+## Send Email to yourself
 e = datetime.datetime.now()
-dt=f"{e.day}, {e.month}, {e.year}"
-tm=f"{e.hour}, {e.minute}, {e.second}"
-subject=f"Login Report from {dt}, {tm}, {foundappointment}"
+dt=f"{e.day}/{e.month}/{e.year}"
+tm=f"{e.hour}:{e.minute}:{e.second}"
+subject=f"Login Report from {dt}---{tm}, Result={foundappointment}"
 body=f"Checked Links {checked_links}"
 send_message(subject=subject,body=body)
 
