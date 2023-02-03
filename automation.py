38af1dc1d07d046da6e42e0523f090a321d4ae61
AsadIsmail <asadismaeel@gmail.com>
2023-02-03 08:25:16 +0000

    Added exception handling

--- automation.py
+++ automation.py
@@ -108,67 +108,84 @@ def find_text(content,txt):
         return True
     else:
         return False
+    
+def save_page(content,err=False):
+    page = content.encode('utf-8')
+    if err:
+        file_ = open('err.html', 'wb')
+        file_.write(page)
+        file_.close()
+    else:
+        file_ = open('found.html', 'wb')
+        file_.write(page)
+        file_.close()
+        
 
 def getcaptha(link,outfile):
-    # To run wothout opening the chrome
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(PATH,options=op)
-    #driver=webdriver.Chrome(PATH)
-    driver.get(link)
-    with open(outfile, 'wb') as file:
-        file.write(driver.find_element(By.XPATH,"//form[@id='appointment_captcha_month']/div/captcha/div").screenshot_as_png)
-    ## Crop unneccassy part
-    img=cv2.imread(outfile)
-    #print(img.shape)
-    assert img is not None, "Input image is None"
-    img=img[:,:300]
-    cv2.imwrite(outfile,img)
-    sleep(1)
-    content=driver.page_source
-    status=find_text(content=content,txt="Please enter here the text you see")
-    assert status==True, "WebPage Could Not be Found!!"
-    
-    ## Run the model and get prediction as string in outstr
-    #driver.close()
-    #outstr=input("Enter the Captcha now!!")
-    tf_img=encode_image(outfile)
-    preds = prediction_model.predict(tf_img)
-    outstr = decode_batch_predictions(preds)[0]
-    #print(pred_texts)
-    #print(f"You entered {inp}")
-    #print(f"Getting Text Box")
-    
-    textbox=driver.find_element(By.ID,"appointment_captcha_month_captchaText")
-    textbox.send_keys(outstr)
-    sleep(3)
-    print(f"Sending predicted Captchas!!")
-    submit = driver.find_element(By.ID,"appointment_captcha_month_appointment_showMonth")
-    submit.click()
-    sleep(5)
-    #content=""
-    content=driver.page_source
-    #print(content)
-    status=find_text(content=content,txt="Please enter here the text you see")
-    foundappoint=find_text(content=content,txt="Unfortunately, there are no appointments")
-    sleep(5)
-    # status 0 capthca read failes, 1 did not found an appointment, 2 found an appointment
-    #print(content)
-    #print(f"Intital status are {status}, {foundappoint}")
-    if status:
-        return 0,"None"
-    if not status and foundappoint:
-        return 1,"None"
-    if not status and not foundappoint:
+    try:
+        # To run wothout opening the chrome
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        driver = webdriver.Chrome(PATH,options=op)
+        #driver=webdriver.Chrome(PATH)
+        driver.get(link)
+        with open(outfile, 'wb') as file:
+            file.write(driver.find_element(By.XPATH,"//form[@id='appointment_captcha_month']/div/captcha/div").screenshot_as_png)
+        ## Crop unneccassy part
+        img=cv2.imread(outfile)
+        #print(img.shape)
+        assert img is not None, "Input image is None"
+        img=img[:,:300]
+        cv2.imwrite(outfile,img)
+        sleep(1)
+        content=driver.page_source
+        status=find_text(content=content,txt="Please enter here the text you see")
+        assert status==True, "WebPage Could Not be Found!!"
+
+        ## Run the model and get prediction as string in outstr
+        #driver.close()
+        #outstr=input("Enter the Captcha now!!")
+        tf_img=encode_image(outfile)
+        preds = prediction_model.predict(tf_img)
+        outstr = decode_batch_predictions(preds)[0]
+        #print(pred_texts)
+        #print(f"You entered {inp}")
+        #print(f"Getting Text Box")
+
+        textbox=driver.find_element(By.ID,"appointment_captcha_month_captchaText")
+        textbox.send_keys(outstr)
+        sleep(3)
+        print(f"Sending predicted Captchas!!")
+        submit = driver.find_element(By.ID,"appointment_captcha_month_appointment_showMonth")
+        submit.click()
+        sleep(2)
+        #content=""
+        content=driver.page_source
         #print(content)
-        soup=BeautifulSoup(content,"html.parser")
-        heading_tags = ["h4"]
-        all_dates=[]
-        for tags in soup.find_all(heading_tags):
-            #print(tags.name + ' -> ' + tags.text.strip())
-            all_dates.append(tags.text.strip())
-        #print(f"All dates are {all_dates}")
-        return 2,' '.join(all_dates)
+        status=find_text(content=content,txt="Please enter here the text you see")
+        foundappoint=find_text(content=content,txt="Unfortunately, there are no appointments")
+        sleep(5)
+        # status 0 capthca read failes, 1 did not found an appointment, 2 found an appointment
+        #print(content)
+        #print(f"Intital status are {status}, {foundappoint}")
+        if status:
+            return 0,"None"
+        if not status and foundappoint:
+            return 1,"None"
+        if not status and not foundappoint:
+            #print(content)
+            save_page(content)
+            soup=BeautifulSoup(content,"html.parser")
+            heading_tags = ["h4"]
+            all_dates=[]
+            for tags in soup.find_all(heading_tags):
+                #print(tags.name + ' -> ' + tags.text.strip())
+                all_dates.append(tags.text.strip())
+            #print(f"All dates are {all_dates}")
+            return 2,' '.join(all_dates)
+    except Exception as e:
+        save_page(content,err=True)
+        return 1,"None"
     
 
 
